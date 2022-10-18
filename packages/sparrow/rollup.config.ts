b8e2696b2bff359d555efa9087c7a706828f02e3
qinghua.cui <qinghua.cui@smartx.com>
2022-10-18 14:57:24 +0800

    fix: update rollup build config

--- packages/sparrow/rollup.config.ts
+++ packages/sparrow/rollup.config.ts
@@ -5,46 +5,58 @@ import dts from "rollup-plugin-dts";
 import esbuild from "rollup-plugin-esbuild";
 import { visualizer } from "rollup-plugin-visualizer";
 
-const config = defineConfig({
-  input: ["index.ts"],
-  plugins: [
-    resolve(),
-    commonjs(),
-    esbuild({
-      include: /\.[jt]sx?$/,
-      exclude: /node_modules/,
-      sourceMap: true,
-      minify: process.env.NODE_ENV === "production",
-      target: "es2017",
-      jsx: "transform",
-      jsxFactory: "React.createElement",
-      jsxFragment: "React.Fragment",
-      define: {},
-      tsconfig: "tsconfig.json",
-      loaders: {
-        ".json": "json",
-        ".js": "jsx",
+const config = defineConfig([
+  {
+    input: ["index.ts"],
+    plugins: [
+      resolve(),
+      commonjs(),
+      esbuild({
+        include: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        sourceMap: true,
+        minify: process.env.NODE_ENV === "production",
+        target: "es2017",
+        jsx: "transform",
+        jsxFactory: "React.createElement",
+        jsxFragment: "React.Fragment",
+        define: {},
+        tsconfig: "tsconfig.json",
+        loaders: {
+          ".json": "json",
+          ".js": "jsx",
+        },
+      }),
+      visualizer({
+        emitFile: true,
+        filename: "stats.html",
+      }),
+    ],
+    external: ["react", "@linaria/core", "react-i18next", "i18next", "antd"],
+    output: [
+      {
+        file: "dist/index.umd.js",
+        name: "index",
+        format: "umd",
       },
-    }),
-    dts() as any,
-    visualizer({
-      emitFile: true,
-      filename: "stats.html",
-    }),
-  ],
-  external: ["react", "@linaria/core", "react-i18next", "i18next", "antd"],
-  output: [
-    {
-      file: "dist/index.umd.js",
-      name: "index",
-      format: "umd",
-    },
-    {
-      file: "dist/index.mjs",
-      name: "index",
-      format: "esm",
-    },
-  ],
-});
+      {
+        file: "dist/index.mjs",
+        name: "index",
+        format: "esm",
+      },
+    ],
+  },
+  {
+    input: ["index.ts"],
+    plugins: [dts() as any],
+    output: [
+      {
+        file: "dist/index.d.ts",
+        name: "index",
+        format: "esm",
+      },
+    ],
+  },
+]);
 
 export default config;
