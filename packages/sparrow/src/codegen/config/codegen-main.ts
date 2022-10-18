e2589d5e8a20c89c2f8f2c94f035188202dbb8f4
qinghua.cui <qinghua.cui@smartx.com>
2022-10-18 15:49:34 +0800

    feat: add images-type generate config

---
+++ packages/sparrow/src/codegen/config/codegen-main.ts
@@ -0,0 +1,71 @@
+const config = {
+  schema: "../../../tower/packages/server/src/generated/schema.graphql",
+  documents: [
+    "../../../tower/packages/ui/src/generated/query.graphql",
+    "../../../tower/packages/ui/src/generated/global-search.graphql",
+    "../../../tower/packages/ui/src/generated/fetch-by-ids.graphql",
+    "../../../tower/packages/ui/src/graphql/*.graphql",
+  ],
+  generates: {
+    // "src/generated/react-hooks.ts": {
+    //   plugins: [
+    //     "typescript",
+    //     "typescript-operations",
+    //     "typescript-react-apollo",
+    //   ],
+    //   config: {
+    //     scalars: {
+    //       DateTime: "string",
+    //       JSON: "{ [key: string]: any }",
+    //     },
+    //     withComponent: false,
+    //     withHOC: false,
+    //     withHooks: true,
+    //     maybeValue: "T | null | undefined",
+    //   },
+    // },
+    // "src/generated/tables.tsx": {
+    //   plugins: ["../claw/dist/plugins/table.js"],
+    // },
+    // "src/generated/selects.tsx": {
+    //   plugins: ["../codegen/dist/react-component/select.js"],
+    // },
+    // "src/generated/forms-types.tsx": {
+    //   plugins: ["../codegen/dist/react-component/form.js"],
+    //   config: {
+    //     generateTypes: true,
+    //   },
+    // },
+    // "src/generated/forms.tsx": {
+    //   plugins: ["../codegen/dist/react-component/form.js"],
+    // },
+    // "src/generated/forms2.tsx": {
+    //   schema: "config/form-schema.graphql",
+    //   plugins: ["../codegen/dist/react-component/form2.js"],
+    // },
+    // "src/generated/type-dependencies-map.ts": {
+    //   plugins: ["../codegen/dist/graphql/type-dependencies-map.js"],
+    // },
+    // "src/generated/global-search.ts": {
+    //   plugins: ["../codegen/dist/typescript/global-search.js"],
+    // },
+    "src/generated/images-type.ts": {
+      plugins: ["../claw/dist/typescript/images-type.js"],
+    },
+    // "src/generated/audit-log-action-options.ts": {
+    //   plugins: ["../claw/dist/typescript/audit-log-action-options.js"],
+    // },
+    // "src/generated/system-audit-log-action-options.ts": {
+    //   plugins: ["../claw/dist/typescript/system-audit-log-action-options.js"],
+    // },
+    // "src/generated/fetch-by-ids.ts": {
+    //   plugins: ["../claw/dist/typescript/fetch-by-ids.js"],
+    // },
+  },
+  config: {
+    path: "src/codegen/config/codegen-config.json",
+    imagesPath: "src/images",
+  },
+};
+
+export default config;
