eab1cf8b4962f7ca74867a78ec5acf10dadc6ed3
qinghua.cui <qinghua.cui@smartx.com>
2022-10-31 15:25:29 +0800

    feat: add antd kit's modal

--- packages/eagle/src/global.d.ts
+++ packages/eagle/src/global.d.ts
@@ -31,3 +31,9 @@ declare module "antd/lib/modal" {
     focusTriggerAfterClose?: boolean;
   }
 }
+
+declare module "antd/lib/modal" {
+  export interface ModalProps {
+    focusTriggerAfterClose?: boolean;
+  }
+}
