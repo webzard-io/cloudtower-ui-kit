97f63729d77b8e3e26505ed48424ead621f3f0c7
qinghua.cui <qinghua.cui@smartx.com>
2022-11-17 14:17:01 +0800

    feat: Add some type

--- packages/eagle/src/kit/specify/index.ts
+++ packages/eagle/src/kit/specify/index.ts
@@ -1,3 +1,4 @@
 export * from "./common";
+export * from "./type";
 export * from "./ui-kit";
 export * from "@cloudtower/eagle/generated/images-type";
---
+++ packages/eagle/src/kit/specify/type.ts
@@ -0,0 +1,3 @@
+import { Dayjs } from "dayjs";
+
+export type DateRange = [Dayjs | null, Dayjs | null];
