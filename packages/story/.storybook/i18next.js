0ee972ddc08ae7414596cd44105cf42e73bbde4d
qinghua.cui <qinghua.cui@smartx.com>
2022-11-24 18:40:20 +0800

    chore: use parrotI18n

--- packages/story/.storybook/i18next.js
+++ packages/story/.storybook/i18next.js
@@ -1,26 +1,3 @@
-import { initReactI18next } from "react-i18next";
-import i18n from "i18next";
-import { zhCN, enUS } from "@cloudtower/parrot";
+import { parrotI18n } from "@cloudtower/parrot";
 
-const resources = {
-  en: {
-    translation: {
-      ...enUS.basic,
-    },
-  },
-  "zh-CN": {
-    translation: {
-      // hello_world: "你好 世界",
-      ...zhCN.basic,
-    },
-  },
-};
-
-i18n.use(initReactI18next).init({
-  debug: true,
-  lng: "zh-CN",
-  fallbackLng: "en",
-  resources,
-});
-
-export default i18n;
+export default parrotI18n;
