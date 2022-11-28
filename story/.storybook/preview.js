86185b9f5ef1d668845ed18e9b76debb69d3c4be
qinghua.cui <qinghua.cui@smartx.com>
2022-11-28 13:18:11 +0800

    chore: import style

--- story/.storybook/preview.js
+++ story/.storybook/preview.js
@@ -1,5 +1,9 @@
 import "antd/dist/antd.css";
 
+import '@cloudtower/eagle/kit/smartx/style.css';
+import '@cloudtower/eagle/styles/reset.css';
+import '@cloudtower/eagle/styles/fonts/font.css';
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
