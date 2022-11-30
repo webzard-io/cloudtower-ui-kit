8ebf5ab8b9998ca2a91aac4f943a0eb14af5c1c9
qinghua.cui <qinghua.cui@smartx.com>
2022-11-30 15:54:12 +0800

    feat: add base64 svg export

---
+++ packages/eagle/src/kit/images/index.ts
@@ -0,0 +1,1 @@
+export { default as svg_1_active_connection_16_gradiendt_blue } from "./1-active-connection-16-gradiendt-blue.svg";
---
+++ story/src/stories/BaseIcon.stories.tsx
@@ -0,0 +1,25 @@
+import { svg_1_active_connection_16_gradiendt_blue } from "@cloudtower/eagle/kit/images";
+import { BaseIcon } from "@cloudtower/eagle/kit/smartx";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
+import React from "react";
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: "Example/BaseIcon",
+  component: BaseIcon,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+} as ComponentMeta<typeof BaseIcon>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof BaseIcon> = (args) => (
+  <BaseIcon {...args} />
+);
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  src: svg_1_active_connection_16_gradiendt_blue,
+};
