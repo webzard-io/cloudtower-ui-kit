2f3487704151818b1e4beb59fc4434eb7ec27b07
qinghua.cui <qinghua.cui@smartx.com>
2022-12-06 19:34:33 +0800

    fix: update BaseIcon's interface

--- packages/eagle/src/kit/smartx/components/BaseIcon.tsx
+++ packages/eagle/src/kit/smartx/components/BaseIcon.tsx
@@ -1,68 +1,28 @@
-import { ImagesType } from "@cloudtower/eagle/generated/images-type";
-import { css } from "@linaria/core";
-import cs from "classnames";
 import _ from "lodash";
 import React from "react";
 
 export interface IBaseIconProps extends React.HTMLAttributes<HTMLSpanElement> {
-  active?: boolean;
-  hoverType?: ImagesType;
-  activeType?: ImagesType;
   className?: string;
   alt?: string;
   width?: number;
   height?: number | "auto";
   cursor?: "pointer" | string;
-  isRotate?: boolean;
   prefixNode?: React.ReactNode;
-  suffixType?: {
-    type: ImagesType;
-    hoverType?: ImagesType;
-    activeType?: ImagesType;
-  };
   suffixIconSrc?: string;
   src: string;
 }
-
-const IconWrapper = css`
-  display: inline-flex;
-  align-items: center;
-
-  .icon-inner {
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-  }
-  .icon-inner + span,
-  span + .icon-inner.suffix {
-    margin-left: 4px;
-  }
-  &.is-rotate {
-    img {
-      animation: rotate 680ms linear infinite;
-    }
-  }
-`;
 
 const BaseIcon = React.forwardRef<HTMLSpanElement, IBaseIconProps>(
   (props, ref) => {
     const {
       alt,
-      hoverType,
-      active,
-      activeType,
-      onMouseEnter,
-      onMouseLeave,
-      onMouseMove,
       className,
       width,
       height,
       cursor,
       style,
       children,
-      isRotate,
       prefixNode,
-      suffixType,
       suffixIconSrc,
       src,
       ...HTMLSpanElementProps
@@ -71,12 +31,7 @@ const BaseIcon = React.forwardRef<HTMLSpanElement, IBaseIconProps>(
     return (
       <span
         ref={ref}
-        className={cs(
-          IconWrapper,
-          "icon-wrapper",
-          className,
-          isRotate && "is-rotate"
-        )}
+        className={className}
         style={_.pickBy({ cursor: cursor, ...style })}
         {...HTMLSpanElementProps}
       >
