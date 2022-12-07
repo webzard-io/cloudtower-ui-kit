6882114042be180b1ce260fc43a849ec39a32148
qinghua.cui <qinghua.cui@smartx.com>
2022-12-07 13:41:47 +0800

    fix: update i18n

--- packages/parrot/src/parrotI18n.ts
+++ packages/parrot/src/parrotI18n.ts
@@ -3,12 +3,25 @@ import i18next from "i18next";
 import locales from "./locales";
 
 const parrotI18n = i18next.createInstance({
-  fallbackLng: "en",
+  lng: "zh-CN",
+  fallbackLng: "en-US",
   debug: true,
-});
-
-parrotI18n.init({
-  resources: locales,
+  interpolation: {
+    prefix: "{",
+    suffix: "}",
+  },
+  resources: {
+    "en-US": {
+      translation: {
+        ...locales["en-US"],
+      },
+    },
+    "zh-CN": {
+      translation: {
+        ...locales["zh-CN"],
+      },
+    },
+  },
 });
 
 export default parrotI18n;
