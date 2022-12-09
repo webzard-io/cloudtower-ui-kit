fba43274b97c71db55974ebcba45a7ba3a644cab
qinghua.cui <qinghua.cui@smartx.com>
2022-12-09 10:26:52 +0800

    chore: update story

--- story/src/stories/Icon.stories.tsx
+++ story/src/stories/Icon.stories.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-  title: "Example/Icon",
+  title: "Icon",
   component: Icon,
   // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
   argTypes: {
