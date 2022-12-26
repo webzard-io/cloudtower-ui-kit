8082366f9c41ac6f860028384f8e00fb08c906ea
qinghua.cui <qinghua.cui@smartx.com>
2022-12-26 09:50:27 +0800

    chore: fix reexport

--- packages/parrot/src/locales/zh-CN/index.ts
+++ packages/parrot/src/locales/zh-CN/index.ts
@@ -1,12 +1,10 @@
 import common from "./common.json";
 import components from "./components.json";
-import error from "./error.json";
 import metric from "./metric.json";
 
 const locale = {
   common: common,
   components: components,
-  error: error,
   metric: metric,
 };
 
