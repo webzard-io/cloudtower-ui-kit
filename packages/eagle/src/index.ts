33da4f08560c1a56cb091eb24bd8a8e527ab83ca
qinghua.cui <qinghua.cui@smartx.com>
2023-02-22 13:47:37 +0800

    TOWER-7925 remove unused export

--- packages/eagle/src/index.ts
+++ packages/eagle/src/index.ts
@@ -1,12 +1,19 @@
 import "./styles/index.scss";
 
-export * from "./components";
-export * from "./hooks";
-export * from "./spec";
-export * from "./store";
-export { default as UIKitProvider } from "./UIKitProvider";
-export * from "./UIKitProvider";
-export * from "./utils";
+export type { DateRange, GraphType, IconProps, IDataPoint } from "./components";
+export {
+  antdKit,
+  KitStoreProvider,
+  tableStyleCover,
+  tickFormatter,
+  useKitDispatch,
+  useKitSelector,
+} from "./components";
+export type { Kit, KitSelectProps, SorterOrder } from "./spec";
+export { kitContext } from "./spec";
+export type { KitRootState } from "./store";
+export { closeModal, popModal, pushModal } from "./store";
+export type { SelectProps } from "./utils";
 export * from "@cloudtower/parrot";
 export type { DropDownProps } from "antd/lib/dropdown";
 export type {
