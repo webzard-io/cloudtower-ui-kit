68bc0c3c8f2ca934a32341511b800c7e11b1ce66
wengxiaojun <xiaojun@smartx.com>
2023-04-27 12:44:41 +0800

    export types and hooks

--- packages/eagle/src/index.ts
+++ packages/eagle/src/index.ts
@@ -3,17 +3,43 @@ import "./styles/index.scss";
 export type { DateRange, GraphType, IconProps, IDataPoint } from "./components";
 export {
   antdKit,
+  Button,
+  FailedLoad,
   KitStoreProvider,
+  PAGINATION_SELECTOR,
+  TABLE_WRAPPER_SELECTOR,
   tableStyleCover,
+  TBODY_SELECTOR,
+  THEAD_SELECTOR,
   tickFormatter,
   useKitDispatch,
   useKitSelector,
 } from "./components";
-export type { Kit, KitSelectProps, SorterOrder } from "./spec";
+export { useElementsSize } from "./hooks";
+export type {
+  DateTimeRangeProps,
+  EnumProps,
+  FloatProps,
+  InputSize,
+  IntProps,
+  Kit,
+  KitSelectProps,
+  RequiredColumnProps,
+  SorterOrder,
+  StringProps,
+  TableProps,
+} from "./spec";
 export { kitContext } from "./spec";
+export * from "./spec/type";
 export type { KitRootState } from "./store";
 export { closeModal, popModal, pushModal } from "./store";
-export type { SelectProps } from "./utils";
+export * from "./UIKitProvider";
+export { default as UIKitProvider } from "./UIKitProvider";
+export type {
+  MultipleSelectProps,
+  SelectProps,
+  SingleSelectProps,
+} from "./utils";
 export * from "@cloudtower/parrot";
 export type { DropDownProps } from "antd/lib/dropdown";
 export type {
