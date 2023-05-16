d48e4c2528356f3ab4f150885330158bc504c657
netweng <miss.wengxiaojun@163.com>
2023-05-16 10:26:11 +0800

    test2

---
+++ packages/icons-react/index.js
@@ -0,0 +1,14 @@
+// The only reason this file exists is to appease Vite's optimizeDeps feature which requires a root-level import.
+
+module.exports = new Proxy(
+  {},
+  {
+    get: (_, property) => {
+      if (property === "__esModule") {
+        return {};
+      }
+
+      throw new Error("Some Import Error");
+    },
+  }
+);
