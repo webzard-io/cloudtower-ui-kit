58a37964ac9b2eac4882c6680eac963cb3b65d26
qinghua.cui <qinghua.cui@smartx.com>
2023-08-09 10:39:59 +0800

    Revert "fix: break circular dep"
    This reverts commit cfaeae9b60d1aca87a307731fa9626aa489aa08f.

--- packages/eagle/src/store/index.ts
+++ packages/eagle/src/store/index.ts
@@ -1,5 +1,7 @@
+import { useCallback } from "react";
 import { combineReducers, createStore } from "redux";
 
+import { useKitDispatch } from "../components/KitStoreProvider";
 import {
   Actions as ChartActionType,
   chartReducer,
@@ -64,6 +66,19 @@ export function pushModal<K extends keyof IModalProps>(
   });
 }
 
+export const usePushModal = () => {
+  const dispatch = useKitDispatch();
+  return useCallback(
+    <K extends keyof IModalProps>(modal: ModalType<IModalProps[K]>) => {
+      dispatch({
+        type: ModalActions.PUSH_MODAL,
+        payload: modal,
+      });
+    },
+    [dispatch]
+  );
+};
+
 /**
  * @deprecated
  */
@@ -72,6 +87,15 @@ export function popModal(store = UIKitStore) {
     type: ModalActions.POP_MODAL,
   });
 }
+
+export const usePopMoal = () => {
+  const dispatch = useKitDispatch();
+  return useCallback(() => {
+    dispatch({
+      type: ModalActions.POP_MODAL,
+    });
+  }, [dispatch]);
+};
 
 /**
  * @deprecated
@@ -82,6 +106,19 @@ export function closeModal(id: number, store = UIKitStore) {
     id,
   });
 }
+
+export const useCloseModal = () => {
+  const dispatch = useKitDispatch();
+  return useCallback(
+    (id: number) => {
+      dispatch({
+        type: ModalActions.CLOSE_MODAL,
+        id,
+      });
+    },
+    [dispatch]
+  );
+};
 
 export type GetModalProps<K extends keyof IModalProps> = IModalProps[K] &
   CloseCb;
