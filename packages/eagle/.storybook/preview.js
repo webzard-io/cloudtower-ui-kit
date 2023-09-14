0c5fa1b79ceaaf5cb4f7a14d008ec71e9814b2de
qinghua.cui <qinghua.cui@smartx.com>
2023-09-14 15:21:31 +0800

    fix: update message group

--- packages/eagle/.storybook/preview.js
+++ packages/eagle/.storybook/preview.js
@@ -4,7 +4,24 @@ import "./overrideRoot.css";
 
 import { initParrotI18n } from "@cloudtower/parrot";
 
-initParrotI18n();
+initParrotI18n({
+  resources: {
+    "en-US": {
+      translation: {
+        test: {
+          error_message_batch: "Batch Error Message {count}",
+        },
+      },
+    },
+    "zh-CN": {
+      translation: {
+        test: {
+          error_message_batch: "错误信息 {count}",
+        },
+      },
+    },
+  },
+});
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
