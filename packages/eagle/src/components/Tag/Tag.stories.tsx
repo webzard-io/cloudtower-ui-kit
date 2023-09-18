e6741ad1bc48222e134bfe237405533cc7190f63
xzdry <dry@justdry.net>
2023-09-18 15:16:16 +0800

    chore(Tag): update tag stories

--- packages/eagle/src/components/Tag/Tag.stories.tsx
+++ packages/eagle/src/components/Tag/Tag.stories.tsx
@@ -6,9 +6,8 @@ import {
   Placeholder16Icon,
 } from "@cloudtower/icons-react";
 import { css } from "@linaria/core";
-import { ComponentStory, Story } from "@storybook/react";
+import { Meta, StoryObj } from "@storybook/react";
 import React from "react";
-import { withDesign } from "storybook-addon-designs";
 
 import { SplitTagComponentType, TagComponentType } from "../../spec";
 import Stack from "../Stack";
@@ -23,246 +22,245 @@ const Title: React.FC = ({ children }) => (
   </div>
 );
 
-const story = {
+const story: Meta<TagComponentType> = {
   title: "Tag",
-  decorators: [withDesign],
+  component: Tag,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/ZE1a32eYk89k4cfGEEOph2/Tag-%26-Token-%7C-%E6%A0%87%E7%AD%BE%E5%92%8C%E5%8F%AF%E7%BC%96%E8%BE%91%E6%A0%87%E7%AD%BE?type=design&node-id=1-41&mode=design&t=FzfpehZkOFyt0tte-0",
+    },
+  },
 };
 
 const modes = ["Default", "Hover", "Default", "Hover"];
 
-export const Basic: ComponentStory<TagComponentType> = () => {
-  return (
-    <div style={{ padding: "20px" }}>
-      <Title>Small</Title>
-      <Stack direction="vertical">
-        <Stack>
-          <span style={{ width: "90px" }} />
-          {modes.map((mode) => (
-            <div style={{ width: "90px" }}>{mode}</div>
+export const Basic: StoryObj<TagComponentType> = {
+  render: () => {
+    return (
+      <div style={{ padding: "20px" }}>
+        <Title>Small</Title>
+        <Stack direction="vertical">
+          <Stack>
+            <span style={{ width: "90px" }} />
+            {modes.map((mode) => (
+              <div style={{ width: "90px" }}>{mode}</div>
+            ))}
+          </Stack>
+          {TagPresetColors.map((color) => (
+            <Stack
+              className={css`
+                & > .box {
+                  width: 90px;
+                }
+              `}
+            >
+              <span style={{ width: "90px" }}>{color}</span>
+              <div className="box">
+                <Tag size="small" color={color}>
+                  Label
+                </Tag>
+              </div>
+              <div className="box">
+                {color.includes("ontint") ? (
+                  <span />
+                ) : (
+                  <Tag
+                    className="__pseudo-states-hover"
+                    size="small"
+                    color={color}
+                  >
+                    Label
+                  </Tag>
+                )}
+              </div>
+              <div className="box">
+                <Tag icon={<Placeholder16Icon />} size="small" color={color}>
+                  Label
+                </Tag>
+              </div>
+              <div className="box">
+                {color.includes("ontint") ? (
+                  <span />
+                ) : (
+                  <Tag
+                    className="__pseudo-states-hover"
+                    icon={<Placeholder16Icon />}
+                    size="small"
+                    color={color}
+                  >
+                    Label
+                  </Tag>
+                )}
+              </div>
+            </Stack>
           ))}
         </Stack>
-        {TagPresetColors.map((color) => (
-          <Stack
-            className={css`
-              & > .box {
-                width: 90px;
-              }
-            `}
-          >
-            <span style={{ width: "90px" }}>{color}</span>
-            <div className="box">
-              <Tag size="small" color={color}>
-                Label
-              </Tag>
-            </div>
-            <div className="box">
-              {color.includes("ontint") ? (
-                <span />
-              ) : (
-                <Tag
-                  className="__pseudo-states-hover"
-                  size="small"
-                  color={color}
-                >
+        <Title>Medium</Title>
+        <Stack direction="vertical">
+          {TagPresetColors.map((color) => (
+            <Stack
+              className={css`
+                & > .box {
+                  width: 90px;
+                }
+              `}
+            >
+              <span style={{ width: "90px" }}>{color}</span>
+              <div className="box">
+                <Tag size="medium" color={color}>
                   Label
                 </Tag>
-              )}
-            </div>
-            <div className="box">
-              <Tag icon={<Placeholder16Icon />} size="small" color={color}>
-                Label
-              </Tag>
-            </div>
-            <div className="box">
-              {color.includes("ontint") ? (
-                <span />
-              ) : (
-                <Tag
-                  className="__pseudo-states-hover"
-                  icon={<Placeholder16Icon />}
-                  size="small"
-                  color={color}
-                >
+              </div>
+              <div className="box">
+                {color.includes("ontint") ? (
+                  <span />
+                ) : (
+                  <Tag
+                    className="__pseudo-states-hover"
+                    size="medium"
+                    color={color}
+                  >
+                    Label
+                  </Tag>
+                )}
+              </div>
+              <div className="box">
+                <Tag icon={<Placeholder16Icon />} size="medium" color={color}>
                   Label
                 </Tag>
-              )}
-            </div>
-          </Stack>
-        ))}
-      </Stack>
-      <Title>Medium</Title>
-      <Stack direction="vertical">
-        {TagPresetColors.map((color) => (
-          <Stack
-            className={css`
-              & > .box {
-                width: 90px;
-              }
-            `}
-          >
-            <span style={{ width: "90px" }}>{color}</span>
-            <div className="box">
-              <Tag size="medium" color={color}>
-                Label
-              </Tag>
-            </div>
-            <div className="box">
-              {color.includes("ontint") ? (
-                <span />
-              ) : (
-                <Tag
-                  className="__pseudo-states-hover"
-                  size="medium"
-                  color={color}
-                >
-                  Label
-                </Tag>
-              )}
-            </div>
-            <div className="box">
-              <Tag icon={<Placeholder16Icon />} size="medium" color={color}>
-                Label
-              </Tag>
-            </div>
-            <div className="box">
-              {color.includes("ontint") ? (
-                <span />
-              ) : (
-                <Tag
-                  className="__pseudo-states-hover"
-                  icon={<Placeholder16Icon />}
-                  size="medium"
-                  color={color}
-                >
-                  Label
-                </Tag>
-              )}
-            </div>
-          </Stack>
-        ))}
-      </Stack>
-    </div>
-  );
-};
-
-Basic.story = {
-  name: "Basic",
-  parameters: {
-    design: {
-      type: "figma",
-      url: "https://www.figma.com/file/nOhhbt8AO1EscJOfx60rfD/%5BArchived%5D-CloudTower-UI-1.0.5?type=design&node-id=405-9&mode=design&t=sD3RRB9kLjiEzIo4-0",
-    },
+              </div>
+              <div className="box">
+                {color.includes("ontint") ? (
+                  <span />
+                ) : (
+                  <Tag
+                    className="__pseudo-states-hover"
+                    icon={<Placeholder16Icon />}
+                    size="medium"
+                    color={color}
+                  >
+                    Label
+                  </Tag>
+                )}
+              </div>
+            </Stack>
+          ))}
+        </Stack>
+      </div>
+    );
   },
 };
 
-export const Custom: Story<{
+export const Custom: StoryObj<{
   content: string;
   color: string;
   size: "small" | "medium";
   hoverable: boolean;
-}> = ({ content, ...props }) => {
-  return <Tag {...props}>{content}</Tag>;
-};
-
-Custom.args = {
-  color: "blue",
-  hoverable: true,
-  content: "label",
-  size: "small",
-};
-
-Custom.argTypes = {
-  size: {
-    control: {
-      type: "select",
+}> = {
+  render: ({ content, ...props }) => {
+    return <Tag {...props}>{content}</Tag>;
+  },
+  args: {
+    color: "blue",
+    hoverable: true,
+    content: "label",
+    size: "small",
+  },
+  argTypes: {
+    size: {
+      control: "select",
       options: ["small", "medium"],
     },
   },
 };
 
-export const SplitTag: ComponentStory<SplitTagComponentType> = () => {
-  return (
-    <div style={{ padding: "20px" }}>
-      <div>Tag.SplitTag</div>
-      <Title>Small</Title>
-      <Stack direction="vertical">
-        {PresetColors.map((color) => (
-          <Stack>
-            <span style={{ display: "inline-block", width: "90px" }}>
-              {color}
-            </span>
-            <Tag.SplitTag
-              primaryContent="Label"
-              secondaryContent="Label"
-              size="small"
-              color={color}
-            />
-            <Tag.SplitTag
-              primaryContent="Label"
-              secondaryContent="Label"
-              icon={<Placeholder16Icon />}
-              size="small"
-              color={color}
-            />
-          </Stack>
-        ))}
-      </Stack>
-      <Title>Medium</Title>
-      <Stack direction="vertical">
-        {PresetColors.map((color) => (
-          <Stack>
-            <span style={{ display: "inline-block", width: "90px" }}>
-              {color}
-            </span>
-            <Tag.SplitTag
-              primaryContent="Label"
-              secondaryContent="Label"
-              size="medium"
-              color={color}
-            />
-            <Tag.SplitTag
-              primaryContent="Label"
-              secondaryContent="Label"
-              icon={<Placeholder16Icon />}
-              size="medium"
-              color={color}
-            />
-          </Stack>
-        ))}
-      </Stack>
-    </div>
-  );
+export const SplitTag: StoryObj<SplitTagComponentType> = {
+  render: () => {
+    return (
+      <div style={{ padding: "20px" }}>
+        <div>Tag.SplitTag</div>
+        <Title>Small</Title>
+        <Stack direction="vertical">
+          {PresetColors.map((color) => (
+            <Stack>
+              <span style={{ display: "inline-block", width: "90px" }}>
+                {color}
+              </span>
+              <Tag.SplitTag
+                primaryContent="Label"
+                secondaryContent="Label"
+                size="small"
+                color={color}
+              />
+              <Tag.SplitTag
+                primaryContent="Label"
+                secondaryContent="Label"
+                icon={<Placeholder16Icon />}
+                size="small"
+                color={color}
+              />
+            </Stack>
+          ))}
+        </Stack>
+        <Title>Medium</Title>
+        <Stack direction="vertical">
+          {PresetColors.map((color) => (
+            <Stack>
+              <span style={{ display: "inline-block", width: "90px" }}>
+                {color}
+              </span>
+              <Tag.SplitTag
+                primaryContent="Label"
+                secondaryContent="Label"
+                size="medium"
+                color={color}
+              />
+              <Tag.SplitTag
+                primaryContent="Label"
+                secondaryContent="Label"
+                icon={<Placeholder16Icon />}
+                size="medium"
+                color={color}
+              />
+            </Stack>
+          ))}
+        </Stack>
+      </div>
+    );
+  },
 };
 
-export const Truncate: Story<{
+export const Truncate: StoryObj<{
   content: string;
   len: number;
   color: string;
   size: "small" | "medium";
-}> = ({ content, len, ...props }) => {
-  return (
-    <>
-      <div>配合 Truncate 组件使用</div>
-      <div style={{ marginTop: "50px" }}>
-        <Tag {...props}>
-          <BaseTruncate backLen={0} text={content} len={len} />
-        </Tag>
-      </div>
-    </>
-  );
-};
-
-Truncate.args = {
-  content: "longlonglonglong",
-  color: "magenta",
-  size: "small",
-  len: 10,
-};
-Truncate.argTypes = {
-  size: {
-    control: {
-      type: "select",
-      options: ["small", "medium"],
+}> = {
+  render: ({ content, len, ...props }) => {
+    return (
+      <>
+        <div>配合 Truncate 组件使用</div>
+        <div style={{ marginTop: "50px" }}>
+          <Tag {...props}>
+            <BaseTruncate backLen={0} text={content} len={len} />
+          </Tag>
+        </div>
+      </>
+    );
+  },
+  args: {
+    content: "longlonglonglong",
+    color: "magenta",
+    size: "small",
+    len: 10,
+  },
+  argTypes: {
+    size: {
+      control: {
+        type: "select",
+        options: ["small", "medium"],
+      },
     },
   },
 };
