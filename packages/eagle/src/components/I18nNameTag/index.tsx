3f7a69018db50d50111406a066f25549774407d6
xzdry <dry@justdry.net>
2023-11-15 16:38:30 +0800

    test

---
+++ packages/eagle/src/components/I18nNameTag/index.tsx
@@ -0,0 +1,32 @@
+import { styled } from "@linaria/react";
+import React from "react";
+
+import { ParrotTrans } from "../../core/ParrotTrans";
+
+export const NameTag = styled.span`
+  font-size: 90%;
+  font-weight: bold;
+  background-color: rgba(240, 243, 247, 0.6);
+  padding: 1px 4px 1px;
+  margin-left: 2px;
+  margin-right: 2px;
+  border: 1px solid rgba(213, 219, 227, 0.6);
+  border-radius: 4px;
+  word-break: break-all;
+`;
+
+// TODO: strict i18nKey type
+const I18nNameTag: React.FC<{
+  name: string;
+  i18nKey: string;
+  [key: string]: unknown;
+}> = (props) => {
+  const { name, i18nKey, ...otherOption } = props;
+  return (
+    <ParrotTrans i18nKey={i18nKey} tOptions={otherOption}>
+      h<NameTag>{name}</NameTag>
+    </ParrotTrans>
+  );
+};
+
+export default I18nNameTag;
