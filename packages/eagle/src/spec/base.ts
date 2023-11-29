f7defde93fae5c0e7acb2d512c73299cf55f9659
xzdry <dry@justdry.net>
2023-11-29 11:25:52 +0800

    refine the i18nNameTag type

--- packages/eagle/src/spec/base.ts
+++ packages/eagle/src/spec/base.ts
@@ -595,7 +595,7 @@ export interface Kit<V = any, T extends HTMLElement = HTMLElement> {
   >;
   token: TokenComponentType;
   overflowTooltip: React.FC<OverflowTooltipProps>;
-  I18nNameTag: React.FC<any>;
+  I18nNameTag: React.FC<I18nNameTagType>;
   uploadDragger: React.ComponentType<DraggerProps>;
   calendar: React.FC<CalendarProps<Moment>>;
   timePicker: React.FC<TimePickerProps>;
@@ -774,3 +774,9 @@ export type TruncateTextWithTooltipType = {
   text: string;
   textWrapperCls?: string;
 } & Omit<TooltipProps, "title">;
+
+export type I18nNameTagType = {
+  name: string;
+  i18nKey: string;
+  [key: string]: unknown;
+};
