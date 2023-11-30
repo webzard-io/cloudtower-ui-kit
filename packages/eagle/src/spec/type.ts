671781d252e17cf966f76a2925b35a667b09e6a9
qinghua.cui <qinghua.cui@smartx.com>
2023-11-30 16:12:01 +0800

    TOWER-10822 Add Breadcrumb

--- packages/eagle/src/spec/type.ts
+++ packages/eagle/src/spec/type.ts
@@ -63,3 +63,17 @@ export interface ICountingProps {
   interval?: number;
   render: () => React.ReactNode;
 }
+
+export interface BreadcrumbLink {
+  name: string;
+  // TODO Attributes that need to be removed
+  id?: string;
+  resource: string;
+  tab?: string;
+
+  onClick: React.MouseEventHandler<HTMLSpanElement>;
+}
+
+export interface IBreadcrumbProps {
+  items: BreadcrumbLink[];
+}
