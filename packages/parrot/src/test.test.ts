c8322584fd2cb1798dd1a4cfdabcfb017a9b451d
wwweng <miss.wengxiaojun@163.com>
2023-11-30 18:01:55 +0800

    update vitest config and tsconfig (#482)

---
+++ packages/parrot/src/test.test.ts
@@ -0,0 +1,18 @@
+import xor from "lodash.xor";
+
+import locales from "./locales";
+
+describe("check missing locales", () => {
+  it("check missing locales", () => {
+    Object.entries(locales["zh-CN"]).forEach((entry) => {
+      let [key, value] = entry;
+      const diff = xor(
+        Object.keys(value),
+        Object.keys(
+          locales["en-US"][key as keyof (typeof locales)["en-US"]] || {},
+        ),
+      );
+      expect(diff).toMatchSnapshot(`${key}.json`);
+    });
+  });
+});
