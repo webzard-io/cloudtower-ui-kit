3ce13f1e48b6f4d40b6c2085ac2bac8081b5e2a3
wwweng <miss.wengxiaojun@163.com>
2023-11-30 18:01:55 +0800

    update vitest config and tsconfig (#482)

--- packages/eagle/src/components/Table/__test__/h5_css.test.tsx
+++ packages/eagle/src/components/Table/__test__/h5_css.test.tsx
@@ -545,16 +545,63 @@ describe("Table h5_css", () => {
 
                   >
                     <div
-                      class=loading
+                      class=
+        width: 64px;
+        height: 64px;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: relative;
+        opacity: 0.2;
+
                     >
                       <div
-                        class=loading-indicator__line1
+                        class=
+        position: absolute;
+        width: 5px;
+        height: 48px;
+        left: 9.33px;
+        top: 40.5px;
+
+        background: linear-gradient(180deg, #0080ff 0%, rgba(0, 128, 255, 0.16) 100%);
+        border-radius: 5px;
+        transform: rotate(-128deg);
+        transform-origin: top left;
+
+        animation: loading-indicator1 1100ms ease-out infinite;
+
                       />
                       <div
-                        class=loading-indicator__line2
+                        class=
+        position: absolute;
+        width: 5px;
+        height: 48px;
+        left: 40.8px;
+        top: 8.4px;
+
+        background: linear-gradient(180deg, #0080ff 0%, rgba(0, 128, 255, 0.16) 100%);
+        border-radius: 5px;
+        transform: rotate(-8deg);
+        transform-origin: top left;
+
+        animation: loading-indicator2 1100ms ease-out infinite;
+
                       />
                       <div
-                        class=loading-indicator__line3
+                        class=
+        position: absolute;
+        width: 5px;
+        height: 48px;
+        left: 52.9px;
+        top: 51.8px;
+
+        background: linear-gradient(180deg, #0080ff 0%, rgba(0, 128, 255, 0.16) 100%);
+        border-radius: 5px;
+        transform: rotate(112deg);
+        transform-origin: top left;
+
+        animation: loading-indicator3 1100ms ease-out infinite;
+
                       />
                     </div>
                   </div>
