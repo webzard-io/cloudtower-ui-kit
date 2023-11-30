b15f65313ab3aaa32f5aac72c13f268c678e37a8
wwweng <miss.wengxiaojun@163.com>
2023-11-30 13:46:12 +0800

    clear useless css files (#479)
    * remove vnc
    
    * remove useless styles
    
    * remove files

--- packages/eagle/src/components/Loading/index.tsx
+++ packages/eagle/src/components/Loading/index.tsx
@@ -2,16 +2,22 @@ import React, { Fragment } from "react";
 
 import { LoadingComponentType } from "../../spec";
 import { FullView } from "../Styled";
+import {
+  LoadingLine1,
+  LoadingLine2,
+  LoadingLine3,
+  LoadingWrapper,
+} from "./style";
 
 const Loading: LoadingComponentType = ({ fullView = true }) => {
   const Wrapper = fullView ? FullView : Fragment;
   const props = fullView ? { className: "loading-full-view" } : {};
   return (
     <Wrapper {...props}>
-      <div className="loading">
-        <div className="loading-indicator__line1"></div>
-        <div className="loading-indicator__line2"></div>
-        <div className="loading-indicator__line3"></div>
+      <div className={LoadingWrapper}>
+        <div className={LoadingLine1}></div>
+        <div className={LoadingLine2}></div>
+        <div className={LoadingLine3}></div>
       </div>
     </Wrapper>
   );
