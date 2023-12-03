b0dbaebbd8ac9e2d2e83cb27999e283f61e97bc5
xzdry <dry@justdry.net>
2023-12-03 16:23:04 +0800

    TOWER-10988 move SwitchWithText

---
+++ packages/eagle/src/coreX/SwitchWithText/index.tsx
@@ -0,0 +1,38 @@
+import { styled } from "linaria/react";
+import React from "react";
+
+import Switch from "../../components/Switch";
+import useParrotTranslation from "../../hooks/useParrotTranslation";
+import { SwitchWithTextProps } from "../../spec";
+
+const SwitchWrapper = styled.span`
+  .enabled-switch {
+    display: flex;
+    align-items: center;
+  }
+
+  .enabled-text {
+    margin-right: 8px;
+  }
+`;
+
+const SwitchWithText: React.FC<SwitchWithTextProps> = (props) => {
+  const { t } = useParrotTranslation();
+  const {
+    text = { checked: t("common.enable"), unchecked: t("common.disable") },
+    ..._props
+  } = props;
+  return (
+    <SwitchWrapper
+      className="enabled-switch"
+      onClick={(e) => e.stopPropagation()}
+    >
+      <span className="enabled-text">
+        {_props.checked ? text.checked : text.unchecked}
+      </span>
+      <Switch {..._props} />
+    </SwitchWrapper>
+  );
+};
+
+export default SwitchWithText;
