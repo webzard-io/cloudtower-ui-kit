28dc7ead308f2be31d6ffaccd321c3059a46c754
xzdry <dry@justdry.net>
2023-12-07 17:46:53 +0800

    TOWER-11040 move GoBackButton

---
+++ packages/eagle/src/coreX/GoBackButton/index.tsx
@@ -0,0 +1,56 @@
+import {
+  ArrowChevronLeft16BoldBlueIcon,
+  ArrowChevronLeft16BoldTertiaryIcon,
+} from "@cloudtower/icons-react";
+import { css, cx } from "@linaria/core";
+import React from "react";
+
+import Icon from "../../components/Icon";
+import { Typo } from "../../components/Typo";
+import { GoBackButtonType } from "../../spec";
+
+const GobackButtonStyle = css`
+  cursor: pointer;
+
+  .link-text {
+    color: $text-light-tertiary;
+  }
+
+  &:hover .link-text {
+    color: $text-light-general;
+  }
+`;
+
+const GobackButton: React.FC<GoBackButtonType> = (props) => {
+  const { onClick, path, index, title, history } = props;
+
+  const goto = () => {
+    if (path) {
+      history.push(path);
+      return;
+    }
+    if (index) {
+      history.go(index);
+      return;
+    }
+    if (onClick) {
+      onClick?.();
+      return;
+    }
+    history.goBack();
+  };
+
+  return (
+    <span className={cx(GobackButtonStyle)}>
+      <Icon
+        src={ArrowChevronLeft16BoldTertiaryIcon}
+        hoverSrc={ArrowChevronLeft16BoldBlueIcon}
+        onClick={goto}
+      >
+        <span className={cx(Typo.Label.l4_bold, "link-text")}>{title}</span>
+      </Icon>
+    </span>
+  );
+};
+
+export default GobackButton;
