f276d5f151ad52680167e005102e7caa13f89638
cqh <cqh963852@hotmail.com>
2024-04-19 15:18:22 +0800

    fix: add store to KitStoreProvider (#671)
    Co-authored-by: qinghua.cui <qinghua.cui@smartx.com>

--- packages/eagle/src/core/KitStoreProvider/index.tsx
+++ packages/eagle/src/core/KitStoreProvider/index.tsx
@@ -1,11 +1,25 @@
-import { Actions, KitRootState, UIKitStore } from "@src/store";
-import React, { createContext, PropsWithChildren } from "react";
+import {
+  Actions,
+  IModalProps,
+  KitRootState,
+  ModalActions,
+  ModalType,
+  rootReducer,
+  UIKitStore,
+} from "@src/store";
+import React, {
+  createContext,
+  PropsWithChildren,
+  useCallback,
+  useState,
+} from "react";
 import {
   createDispatchHook,
   createSelectorHook,
   Provider,
   ReactReduxContextValue,
 } from "react-redux";
+import { createStore } from "redux";
 
 const ctx = createContext<ReactReduxContextValue<KitRootState, Actions>>({
   store: UIKitStore,
@@ -17,8 +31,10 @@ interface IProps {}
 const KitStoreProvider = (props: PropsWithChildren<IProps>) => {
   const { children } = props;
 
+  const [store] = useState(createStore(rootReducer));
+
   return (
-    <Provider context={ctx} store={UIKitStore}>
+    <Provider context={ctx} store={store}>
       {children}
     </Provider>
   );
@@ -28,3 +44,40 @@ export default KitStoreProvider;
 
 export const useKitDispatch = createDispatchHook<KitRootState, Actions>(ctx);
 export const useKitSelector = createSelectorHook<KitRootState, Actions>(ctx);
+export const usePushModal = () => {
+  const dispatch = useKitDispatch();
+  const pushModal = useCallback(
+    <K extends keyof IModalProps>(modal: ModalType<IModalProps[K]>) => {
+      dispatch({
+        type: ModalActions.PUSH_MODAL,
+        payload: modal,
+      });
+    },
+    [dispatch],
+  );
+  return pushModal;
+};
+
+export const useCloseModal = () => {
+  const dispatch = useKitDispatch();
+  const closeModal = useCallback(
+    (id: number) => {
+      dispatch({
+        type: ModalActions.CLOSE_MODAL,
+        id,
+      });
+    },
+    [dispatch],
+  );
+  return closeModal;
+};
+
+export const usePopModal = () => {
+  const dispatch = useKitDispatch();
+  const popModal = useCallback(() => {
+    dispatch({
+      type: ModalActions.POP_MODAL,
+    });
+  }, [dispatch]);
+  return popModal;
+};
