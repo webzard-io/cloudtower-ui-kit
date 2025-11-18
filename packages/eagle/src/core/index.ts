c2f7f90989035c4298e110004e48c20997a7f87f
chenenpei <enpei.chen@smartx.com>
2025-11-18 17:14:30 +0800

    feat(Upload): add Upload component

--- packages/eagle/src/core/index.ts
+++ packages/eagle/src/core/index.ts
@@ -101,6 +101,7 @@ export * from "./Tooltip";
 export * from "./Truncate";
 export * from "./Typo";
 export * from "./Units";
+export * from "./Upload";
 export * from "./WizardDialog";
 
 export const units = {
