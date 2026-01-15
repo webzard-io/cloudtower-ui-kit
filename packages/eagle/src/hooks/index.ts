c58e8f410fa2ed4a98eb2bfe1c524b082311c9f3
wengxiaojun <xiaojun@smartx.com>
2026-01-15 12:01:16 +0800

    TOWER-21477: feat(Duration): add Duration component for formatting and displaying time durations with multiple units

--- packages/eagle/src/hooks/index.ts
+++ packages/eagle/src/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from "./useCTErrorMsg";
 export { default as useElementResize } from "./useElementResize";
 export { default as useElementsSize } from "./useElementsSize";
+export { useFormatDuration } from "./useFormatDuration";
