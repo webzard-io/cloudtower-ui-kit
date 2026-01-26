9fea33e060094a3e191dc056cc3803eee6ebe00c
MrWindlike <MrWindlike@gmail.com>
2026-01-26 15:14:15 +0800

    feat: change the colors of link (#1225)

--- packages/eagle/.storybook/main.ts
+++ packages/eagle/.storybook/main.ts
@@ -9,7 +9,7 @@ const VariableSassPath = path.resolve(
   __dirname,
   "../src/styles/common/variables.scss",
 );
-
+const ColorSassPath = path.resolve(__dirname, "../src/styles/token/color.scss");
 const AnimationPath = path.resolve(
   __dirname,
   "../src/styles/common/animation.scss",
@@ -45,6 +45,9 @@ const config: StorybookConfig = {
   },
   async viteFinal(config) {
     const varData = fs.readFileSync(VariableSassPath, {
+      encoding: "utf-8",
+    });
+    const colorData = fs.readFileSync(ColorSassPath, {
       encoding: "utf-8",
     });
     const animationData = fs.readFileSync(AnimationPath, {
@@ -83,6 +86,8 @@ const config: StorybookConfig = {
               .renderSync({
                 data: [
                   varData,
+                  `\n`,
+                  colorData,
                   `\n`,
                   animationData,
                   `${selector} { ${cssText} }`,
