42a64b670d43060e9e821314926e06243f6f0203
Jiaxi Zhong <jiaxi.zhong@smartx.com>
2026-07-31 17:43:26 +0800

    TOWER-23349: align Antd5Tree indeterminate checkbox style

--- packages/eagle/src/core/Antd5Tree/Antd5Tree.tsx
+++ packages/eagle/src/core/Antd5Tree/Antd5Tree.tsx
@@ -1,1 +1,37 @@
-export { Tree as Antd5Tree } from "antd5";
+import { cx } from "@linaria/core";
+import { Antd5PrefixCls } from "@src/utils";
+import { Tree as BaseAntd5Tree } from "antd5";
+import React from "react";
+
+import { Antd5TreeStyle } from "./Antd5Tree.style";
+import { Antd5TreeProps } from "./Antd5Tree.type";
+
+const InternalAntd5Tree = React.forwardRef<
+  React.ComponentRef<typeof BaseAntd5Tree>,
+  Antd5TreeProps
+>(({ className, ...props }, ref) => (
+  <BaseAntd5Tree
+    {...props}
+    className={cx(Antd5TreeStyle, className)}
+    prefixCls={`${Antd5PrefixCls}-tree`}
+    ref={ref}
+  />
+));
+
+const InternalAntd5DirectoryTree = React.forwardRef<
+  React.ComponentRef<typeof BaseAntd5Tree.DirectoryTree>,
+  React.ComponentPropsWithoutRef<typeof BaseAntd5Tree.DirectoryTree>
+>(({ className, ...props }, ref) => (
+  <BaseAntd5Tree.DirectoryTree
+    {...props}
+    className={cx(Antd5TreeStyle, className)}
+    prefixCls={`${Antd5PrefixCls}-tree`}
+    ref={ref}
+  />
+));
+
+export const Antd5Tree = Object.assign(InternalAntd5Tree, {
+  DirectoryTree:
+    InternalAntd5DirectoryTree as typeof BaseAntd5Tree.DirectoryTree,
+  TreeNode: BaseAntd5Tree.TreeNode,
+}) as typeof BaseAntd5Tree;
